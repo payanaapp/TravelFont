19865c05bf90a706a88da2ba227b13e67befe46c
Abhinandan Kelgere Ramesh <abhinandankelgereramesh@Abhinandans-MacBook-Pro.local>
2022-11-03 00:25:59 -0700

    Payana travel footprint

---
+++ payana/payana_bl/bigtable_utils/PayanaProfileTravelFootPrint.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+"""Demonstrates how to write ProfileInfo into BigTable
+"""
+
+import argparse
+import random
+import json
+import time
+import hashlib
+from payana.payana_bl.bigtable_utils.constants import bigtable_constants
+from payana.payana_bl.bigtable_utils.PayanaBigTable import PayanaBigTable
+from payana.payana_bl.bigtable_utils.bigtable_read_write_object_wrapper import bigtable_write_object_wrapper
+from payana.payana_bl.common_utils.payana_exception_handler_utils import payana_generic_exception_handler
+from payana.payana_bl.bigtable_utils.constants import bigtable_constants
+
+# google cloud bigtable imports
+from google.cloud.bigtable import column_family
+
+
+class PayanaProfileTravelFootPrint:
+
+    @payana_generic_exception_handler
+    def __init__(self, profile_id, place_id, excursion_id, latitude, longitude):
+
+        self.payana_profile_page_travel_footprint_profile_id = bigtable_constants.payana_profile_page_travel_footprint_profile_id
+        self.payana_profile_page_travel_footprint_place_id = bigtable_constants.payana_profile_page_travel_footprint_place_id
+        self.payana_profile_page_travel_footprint_excursion_id = bigtable_constants.payana_profile_page_travel_footprint_excursion_id
+        self.payana_profile_page_travel_footprint_latitude = bigtable_constants.payana_profile_page_travel_footprint_latitude
+        self.payana_profile_page_travel_footprint_longitude = bigtable_constants.payana_profile_page_travel_footprint_longitude
+        self.payana_profile_page_travel_footprint_column_family = bigtable_constants.payana_profile_page_travel_footprint_column_family
+
+        self.payana_profile_page_travel_footprint_column_qualifier = ""
+
+        self.payana_profile_page_travel_footprint_column_value = ""
+
+        self.profile_id = profile_id
+        self.place_id = place_id
+        self.excursion_id = excursion_id
+        self.latitude = latitude
+        self.longitude = longitude
+
+        self.update_bigtable_write_objects = []
+
+    @payana_generic_exception_handler
+    def toJSON(self):
+        return self.__dict__
+
+    @payana_generic_exception_handler
+    def generate_travel_footprint_column_qualifier(self):
+
+        self.payana_profile_page_travel_footprint_column_qualifier = "##".join(
+            [str(self.place_id), str(self.excursion_id)])
+
+        self.payana_profile_page_travel_footprint_column_value = "##".join(
+            [str(self.latitude), str(self.longitude)])
+
+    @payana_generic_exception_handler
+    def update_profile_travel_footprint_bigtable(self):
+
+        payana_profile_table_instance = PayanaBigTable(
+            bigtable_constants.payana_profile_travel_footprint_table)
+        
+        self.generate_travel_footprint_column_qualifier()
+        
+        self.create_bigtable_write_objects()
+
+        return payana_profile_table_instance.insert_columns(
+            self.update_bigtable_write_objects)
+
+    @payana_generic_exception_handler
+    def create_bigtable_write_objects(self):
+        self.set_profile_travel_footprint_write_object()
+
+    @payana_generic_exception_handler
+    def set_profile_travel_footprint_write_object(self):
+
+        # profile_name write object
+        self.update_bigtable_write_objects.append(bigtable_write_object_wrapper(
+            self.profile_id, self.payana_profile_page_travel_footprint_column_family, self.payana_profile_page_travel_footprint_column_qualifier, self.payana_profile_page_travel_footprint_column_value))
---
+++ payana_profile_page_travel_footprint.py
@@ -0,0 +1,160 @@
+from datetime import datetime
+
+from payana.payana_bl.bigtable_utils.payana_bigtable_init import payana_bigtable_init
+from payana.payana_bl.bigtable_utils.payana_bigtable_cleanup import payana_bigtable_cleanup
+from payana.payana_bl.bigtable_utils.bigtable_read_write_object_wrapper import bigtable_write_object_wrapper, bigtable_read_row_key_wrapper
+from payana.payana_bl.bigtable_utils.constants import bigtable_constants
+from payana.payana_bl.bigtable_utils.PayanaProfileTable import PayanaProfileTable
+from payana.payana_bl.bigtable_utils.PayanaBigTable import PayanaBigTable
+from payana.payana_bl.bigtable_utils.PayanaCommentsTable import PayanaCommentsTable
+from payana.payana_bl.bigtable_utils.PayanaExcursionTable import PayanaExcursionTable
+from payana.payana_bl.bigtable_utils.PayanaItineraryTable import PayanaItineraryTable
+from payana.payana_bl.bigtable_utils.PayanaCheckinTable import PayanaCheckinTable
+from payana.payana_bl.bigtable_utils.PayanaLikesTable import PayanaLikesTable
+from payana.payana_bl.bigtable_utils.PayanaTravelBuddyTable import PayanaTravelBuddyTable
+from payana.payana_bl.bigtable_utils.PayanaPlaceIdMetadataTable import PayanaPlaceIdMetadataTable
+from payana.payana_bl.bigtable_utils.PayanaNeighboringCitiesTable import PayanaNeighboringCitiesTable
+from payana.payana_bl.bigtable_utils.PayanaStateTable import PayanaStateTable
+from payana.payana_bl.bigtable_utils.PayanaCountryTable import PayanaCountryTable
+from payana.payana_bl.bigtable_utils.PayanaPersonalPlaceIdItineraryTable import PayanaPersonalPlaceIdItineraryTable
+from payana.payana_bl.bigtable_utils.PayanaPersonalCityItineraryTable import PayanaPersonalCityItineraryTable
+from payana.payana_bl.bigtable_utils.PayanaPersonalStateItineraryTable import PayanaPersonalStateItineraryTable
+from payana.payana_bl.bigtable_utils.PayanaPersonalCountryItineraryTable import PayanaPersonalCountryItineraryTable
+from payana.payana_bl.bigtable_utils.PayanaProfileTravelFootPrint import PayanaProfileTravelFootPrint
+client_config_file_path = bigtable_constants.bigtable_client_config_path
+bigtable_tables_schema_path = bigtable_constants.bigtable_schema_config_file
+
+payana_bigtable_init(client_config_file_path, bigtable_tables_schema_path)
+
+payana_profile_page_travel_footprint_profile_id = bigtable_constants.payana_profile_page_travel_footprint_profile_id
+payana_profile_page_travel_footprint_place_id = bigtable_constants.payana_profile_page_travel_footprint_place_id
+payana_profile_page_travel_footprint_excursion_id = bigtable_constants.payana_profile_page_travel_footprint_excursion_id
+payana_profile_page_travel_footprint_latitude = bigtable_constants.payana_profile_page_travel_footprint_latitude
+payana_profile_page_travel_footprint_longitude = bigtable_constants.payana_profile_page_travel_footprint_longitude
+payana_profile_page_travel_footprint_column_family = bigtable_constants.payana_profile_page_travel_footprint_column_family
+
+profile_travel_footprint_obj = {
+    "profile_id": "123456789",
+    "place_id": "12345",
+    "excursion_id": "678910",
+    "latitude": "1.234",
+    "longitude": "2.3456"
+}
+
+payana_profile_travel_footprint_obj = PayanaProfileTravelFootPrint(
+    **profile_travel_footprint_obj)
+payana_profile_travel_footprint_obj_write_status = payana_profile_travel_footprint_obj.update_profile_travel_footprint_bigtable()
+
+print("payana_profile_travel_footprint_obj_write_status: " +
+      str(payana_profile_travel_footprint_obj_write_status))
+
+profile_id = payana_profile_travel_footprint_obj.profile_id
+payana_profile_travel_footprint_table = bigtable_constants.payana_profile_travel_footprint_table
+payana_profile_travel_footprint_read_obj = PayanaBigTable(
+    payana_profile_travel_footprint_table)
+payana_profile_travel_footprint_obj_read = payana_profile_travel_footprint_read_obj.get_row_dict(
+    profile_id, include_column_family=True)
+print(payana_profile_travel_footprint_obj_read)
+
+print("Addition of a new travel footprint profile object: " +
+      str(payana_profile_travel_footprint_obj_read != None))
+
+# Change latitude
+new_latitude = "56789.870"
+
+place_id = profile_travel_footprint_obj["place_id"]
+excursion_id = profile_travel_footprint_obj["excursion_id"]
+latitude = profile_travel_footprint_obj["latitude"]
+longitude = profile_travel_footprint_obj["longitude"]
+
+payana_profile_page_travel_footprint_column_qualifier = "##".join(
+    [str(place_id), str(excursion_id)])
+
+payana_profile_page_travel_footprint_column_value = "##".join(
+    [str(new_latitude), str(longitude)])
+
+profile_table_profile_name_write_object = bigtable_write_object_wrapper(
+    profile_id, payana_profile_page_travel_footprint_column_family, payana_profile_page_travel_footprint_column_qualifier, payana_profile_page_travel_footprint_column_value)
+
+payana_profile_travel_footprint_obj_update_status = payana_profile_travel_footprint_read_obj.insert_column(
+    profile_table_profile_name_write_object)
+
+print("payana_profile_travel_footprint_obj_update_status: " +
+      str(payana_profile_travel_footprint_obj_update_status))
+
+profile_table_profile_travel_footprint_update = payana_profile_travel_footprint_read_obj.get_row_dict(
+    profile_id, include_column_family=True)
+
+updated_profile_travel_footprint_obj = profile_table_profile_travel_footprint_update[profile_id][
+    payana_profile_page_travel_footprint_column_family][payana_profile_page_travel_footprint_column_qualifier]
+
+updated_latitude, updated_longitude = updated_profile_travel_footprint_obj.split(
+    "##")
+
+print("Status of update profile travel footprint operation: " +
+      str(new_latitude == updated_latitude))
+
+# Add a new place obj
+place_id = "919931"
+excursion_id = "09876"
+latitude = "890.01"
+longitude = "6415.01"
+
+payana_profile_page_travel_footprint_column_qualifier = "##".join(
+    [str(place_id), str(excursion_id)])
+
+payana_profile_page_travel_footprint_column_value = "##".join(
+    [str(latitude), str(longitude)])
+
+profile_table_profile_name_write_object_updated = bigtable_write_object_wrapper(
+    profile_id, payana_profile_page_travel_footprint_column_family, payana_profile_page_travel_footprint_column_qualifier, payana_profile_page_travel_footprint_column_value)
+payana_profile_travel_footprint_obj_update_status = payana_profile_travel_footprint_read_obj.insert_column(
+    profile_table_profile_name_write_object_updated)
+
+print("payana_profile_travel_footprint_obj_update_status: " +
+      str(payana_profile_travel_footprint_obj_update_status))
+
+profile_table_profile_travel_footprint_update = payana_profile_travel_footprint_read_obj.get_row_dict(
+    profile_id, include_column_family=True)
+
+updated_profile_travel_footprint_obj = profile_table_profile_travel_footprint_update[profile_id][
+    payana_profile_page_travel_footprint_column_family][payana_profile_page_travel_footprint_column_qualifier]
+
+updated_latitude, updated_longitude = updated_profile_travel_footprint_obj.split(
+    "##")
+
+print("Status of update profile travel footprint operation: " +
+      str(latitude == updated_latitude))
+
+# Delete a travel footprint column
+payana_profile_obj_delete_footprint_status = payana_profile_travel_footprint_read_obj.delete_bigtable_row_column(
+    profile_table_profile_name_write_object_updated)
+
+print("payana_profile_obj_delete_footprint_status: " +
+      str(payana_profile_obj_delete_footprint_status))
+
+profile_table_travel_footprint_delete = payana_profile_travel_footprint_read_obj.get_row_dict(
+    profile_id, include_column_family=True)
+
+updated_profile_travel_footprint_obj = profile_table_travel_footprint_delete[profile_id][
+    payana_profile_page_travel_footprint_column_family]
+
+print("Status of delete travel footprint operation: " +
+      str(payana_profile_page_travel_footprint_column_qualifier not in updated_profile_travel_footprint_obj))
+
+# Remove the whole row
+profile_travel_footprint_row_delete_object = bigtable_write_object_wrapper(
+    profile_id, "", "", "")
+profile_travel_footprint_row_delete_status = payana_profile_travel_footprint_read_obj.delete_bigtable_row(
+    profile_travel_footprint_row_delete_object)
+
+print("profile_travel_footprint_row_delete_status: " +
+      str(profile_travel_footprint_row_delete_status))
+
+profile_obj_read_travel_footprint_update = payana_profile_travel_footprint_read_obj.get_row_dict(
+    profile_id, include_column_family=True)
+
+print("Status of profile travel footprint delete row:" +
+      str(len(profile_obj_read_travel_footprint_update) == 0))
+
+payana_bigtable_cleanup(client_config_file_path, bigtable_tables_schema_path)
