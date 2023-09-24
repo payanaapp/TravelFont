d7c0bdd01a2321d4c9edea80cc4c5cb469af07a3
Abhinandan Kelgere Ramesh <abhinandankelgereramesh@Abhinandans-MacBook-Pro.local>
2023-09-23 18:48:28 -0700

    Gunicorn server changes

--- payana/payana_service/payana_service_main.py
+++ payana/payana_service/payana_service_main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, Blueprint
-from payana.payana_service.server import payana_bigtable_server_init
-
+from payana.payana_service.server import payana_bigtable_server_init, payana_bigtable_gunicorn_server_init
 
 def run():
     payana_bigtable_server_init.initialize_app()
+    
+def gunicorn_run():
+    payana_bigtable_server_init.gunicorn_initialize_app()
     
 def main():
     payana_bigtable_server_init.initialize_app()
